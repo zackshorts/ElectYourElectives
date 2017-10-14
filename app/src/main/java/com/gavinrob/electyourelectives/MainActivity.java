7dbe4dfddb16622e9601165b67eb3dbd7f06c345
DESKTOP-TI81K93\gavin <gavinrob92@gmail.com>
2017-10-14 14:44:32 -0600

    Better spinners and floating button disabled until selection made

--- app/src/main/java/com/gavinrob/electyourelectives/MainActivity.java
+++ app/src/main/java/com/gavinrob/electyourelectives/MainActivity.java
@@ -1,47 +1,127 @@
 package com.gavinrob.electyourelectives;
 
 import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private FloatingActionButton fab;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         //get the spinner from the xml.
-        Spinner dropdown = (Spinner)findViewById(R.id.degreeSpinner);
+        Spinner degreeSpinner = (Spinner)findViewById(R.id.degreeSpinner);
         String[] items = new String[]{"Select your Degree","Computer Science", "Information Systems", "Information Technology"};
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, items){
+            @Override
+            public boolean isEnabled(int position){
+                if(position == 0){return false;}
+                else{return true;}
+            }
+            @Override
+            public View getDropDownView(int position, View convertView,
+                                        ViewGroup parent) {
+                View view = super.getDropDownView(position, convertView, parent);
+                TextView tv = (TextView) view;
+                ((TextView) view).setGravity(Gravity.CENTER);
+                if(position == 0){tv.setTextColor(Color.GRAY);}
+                else {tv.setTextColor(Color.BLACK);}
+                return view;
+            }
+        };
         adapter.setDropDownViewResource(R.layout.spinner_text);
-        dropdown.setAdapter(adapter);
+        degreeSpinner.setAdapter(adapter);
+        degreeSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                changeColor();
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                changeColor();
+            }
+        });
 
         //get the spinner from the xml.
-        Spinner dropdown1 = (Spinner)findViewById(R.id.universitySpinner);
+        Spinner universitySpinner = (Spinner)findViewById(R.id.universitySpinner);
         String[] items1 = new String[]{"Select your University","Brigham Young University", "Utah Valley University", "University of Utah"};
-        ArrayAdapter<String> adapter1 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items1);
+        ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, items1){
+            @Override
+            public boolean isEnabled(int position){
+                if(position == 0){return false;}
+                else{return true;}
+            }
+            @Override
+            public View getDropDownView(int position, View convertView,
+                                        ViewGroup parent) {
+                View view = super.getDropDownView(position, convertView, parent);
+                TextView tv = (TextView) view;
+                ((TextView) view).setGravity(Gravity.CENTER);
+                if(position == 0){tv.setTextColor(Color.GRAY);}
+                else {tv.setTextColor(Color.BLACK);}
+                return view;
+            }
+        };
         adapter1.setDropDownViewResource(R.layout.spinner_text);
-        dropdown1.setAdapter(adapter1);
+        universitySpinner.setAdapter(adapter1);
+        universitySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                changeColor();
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                changeColor();
+            }
+        });
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.hide();
+
+    }
+
+    public void changeColor(){
+        System.out.printf("here");
+        if(canProceed()){
+            fab.show();
+        }
+    }
+
+    public boolean canProceed(){
+        Spinner degreeSpin = (Spinner)findViewById(R.id.degreeSpinner);
+        Spinner unSpin = (Spinner)findViewById(R.id.universitySpinner);
+        if(degreeSpin.getSelectedItem().toString() != "Select your Degree" && unSpin.getSelectedItem().toString() != "Select your University"){
+            return true;
+        }
+        return false;
     }
 
     public void proceedToFilters(View view) {
-        Intent intent = new Intent(this, FiltersActivity.class);
-
-
-        startActivity(intent);
+        if(canProceed()){
+            Intent intent = new Intent(this, FiltersActivity.class);
+            startActivity(intent);
+        }
     }
 
     @Override
