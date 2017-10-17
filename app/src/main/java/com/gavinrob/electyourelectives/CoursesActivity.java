b3439418c3b0923fc7933c96f11b5477d85ecae4
DESKTOP-TI81K93\gavin <gavinrob92@gmail.com>
2017-10-17 11:39:05 -0600

    Added in RecyclerView

---
+++ app/src/main/java/com/gavinrob/electyourelectives/CoursesActivity.java
@@ -0,0 +1,57 @@
+package com.gavinrob.electyourelectives;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.Window;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class CoursesActivity extends AppCompatActivity  {
+
+    Context context;
+    RecyclerView recyclerView;
+    RelativeLayout relativeLayout;
+    RecyclerView.Adapter recyclerViewAdapter;
+    RecyclerView.LayoutManager recylerViewLayoutManager;
+    String[] subjects =
+            {
+                    "ANDROID",
+                    "PHP",
+                    "BLOGGER",
+                    "WORDPRESS",
+                    "JOOMLA",
+                    "ASP.NET",
+                    "JAVA",
+                    "C++",
+                    "MATHS",
+                    "HINDI",
+                    "ENGLISH"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_ACTION_BAR);
+
+        setContentView(R.layout.activity_courses);
+
+        context = getApplicationContext();
+
+        relativeLayout = (RelativeLayout) findViewById(R.id.relativelayout1);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerview1);
+
+        recylerViewLayoutManager = new LinearLayoutManager(context);
+
+        recyclerView.setLayoutManager(recylerViewLayoutManager);
+
+        recyclerViewAdapter = new RecyclerViewAdapter(context, subjects);
+
+        recyclerView.setAdapter(recyclerViewAdapter);
+    }
+}
--- app/src/main/java/com/gavinrob/electyourelectives/FiltersActivity.java
+++ app/src/main/java/com/gavinrob/electyourelectives/FiltersActivity.java
@@ -3,6 +3,7 @@ package com.gavinrob.electyourelectives;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class FiltersActivity extends AppCompatActivity {
 
@@ -12,5 +13,10 @@ public class FiltersActivity extends AppCompatActivity {
         setContentView(R.layout.activity_filters);
 
         Intent intent = getIntent();
+    }
+
+    public void proceedToCourses(View view) {
+            Intent intent = new Intent(this, CoursesActivity.class);
+            startActivity(intent);
     }
 }
