82519291ac9dbdf220cc84da11e3799ceaa53101
DESKTOP-TI81K93\gavin <gavinrob92@gmail.com>
2017-10-22 20:48:36 -0600

    Added aditional data to course class

--- app/src/main/java/com/gavinrob/electyourelectives/Course.java
+++ app/src/main/java/com/gavinrob/electyourelectives/Course.java
@@ -6,9 +6,81 @@ package com.gavinrob.electyourelectives;
 
 public class Course {
     private String name;
+    private String description;
 
-    public Course(String name){
+    private boolean isJava = false;
+    private boolean isCpp = false;
+    private boolean isC = false;
+    private boolean isJs = false;
+    private boolean isPython = false;
+    private boolean isAndroid = false;
+    private boolean isCSharp = false;
+    private boolean isAny = false;
+
+    private boolean isMobile = false;
+    private boolean isWeb = false;
+    private boolean isSecurity = false;
+    private boolean isAnimation = false;
+    private boolean isBio = false;
+    private boolean isOther = false;
+
+    //can be 0 - 5 (intially set to 5)
+    private int difficulty = 5;
+    private int coding = 5;
+    private int usefulness = 5;
+
+
+    public Course(String name, String language, String type, String description){
         this.name = name;
+        this.description = description;
+
+        switch(language){
+            case "java":
+                isJava = true;
+                break;
+            case "cpp":
+                isCpp = true;
+                break;
+            case "c":
+                isC = true;
+                break;
+            case "js":
+                isJs = true;
+                break;
+            case "python":
+                isPython = true;
+                break;
+            case "android":
+                isAndroid = true;
+                break;
+            case "cSharp":
+                isCSharp = true;
+                break;
+            case "any":
+                isAny = true;
+                break;
+        }
+
+        switch (type){
+            case "mobile":
+                isMobile = true;
+                break;
+            case "web":
+                isWeb = true;
+                break;
+            case "security":
+                isSecurity = true;
+                break;
+            case "animation":
+                isAnimation = true;
+                break;
+            case "bio":
+                isBio = true;
+                break;
+            case "other":
+                isOther = true;
+                break;
+        }
     }
 
     public String getName() {
@@ -17,5 +89,149 @@ public class Course {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public boolean isJava() {
+        return isJava;
+    }
+
+    public void setJava(boolean java) {
+        isJava = java;
+    }
+
+    public boolean isCpp() {
+        return isCpp;
+    }
+
+    public void setCpp(boolean cpp) {
+        isCpp = cpp;
+    }
+
+    public boolean isC() {
+        return isC;
+    }
+
+    public void setC(boolean c) {
+        isC = c;
+    }
+
+    public boolean isJs() {
+        return isJs;
+    }
+
+    public void setJs(boolean js) {
+        isJs = js;
+    }
+
+    public boolean isPython() {
+        return isPython;
+    }
+
+    public void setPython(boolean python) {
+        isPython = python;
+    }
+
+    public boolean isAndroid() {
+        return isAndroid;
+    }
+
+    public void setAndroid(boolean android) {
+        isAndroid = android;
+    }
+
+    public boolean isCSharp() {
+        return isCSharp;
+    }
+
+    public void setCSharp(boolean CSharp) {
+        isCSharp = CSharp;
+    }
+
+    public boolean isAny() {
+        return isAny;
+    }
+
+    public void setAny(boolean any) {
+        isAny = any;
+    }
+
+    public boolean isMobile() {
+        return isMobile;
+    }
+
+    public void setMobile(boolean mobile) {
+        isMobile = mobile;
+    }
+
+    public boolean isWeb() {
+        return isWeb;
+    }
+
+    public void setWeb(boolean web) {
+        isWeb = web;
+    }
+
+    public boolean isSecurity() {
+        return isSecurity;
+    }
+
+    public void setSecurity(boolean security) {
+        isSecurity = security;
+    }
+
+    public boolean isAnimation() {
+        return isAnimation;
+    }
+
+    public void setAnimation(boolean animation) {
+        isAnimation = animation;
+    }
+
+    public boolean isBio() {
+        return isBio;
+    }
+
+    public void setBio(boolean bio) {
+        isBio = bio;
+    }
+
+    public boolean isOther() {
+        return isOther;
+    }
+
+    public void setOther(boolean other) {
+        isOther = other;
+    }
+
+    public int getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(int difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public int getCoding() {
+        return coding;
+    }
+
+    public void setCoding(int coding) {
+        this.coding = coding;
+    }
+
+    public int getUsefulness() {
+        return usefulness;
+    }
+
+    public void setUsefulness(int usefulness) {
+        this.usefulness = usefulness;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
--- app/src/main/java/com/gavinrob/electyourelectives/CoursesActivity.java
+++ app/src/main/java/com/gavinrob/electyourelectives/CoursesActivity.java
@@ -20,24 +20,24 @@ public class CoursesActivity extends AppCompatActivity  {
     RelativeLayout relativeLayout;
     RecyclerView.Adapter recyclerViewAdapter;
     RecyclerView.LayoutManager recylerViewLayoutManager;
-    Course cs260 = new Course("CS345");
-    Course cs330 = new Course("CS330");
-    Course cs345 = new Course("CS345");
-    Course cs355 = new Course("CS355");
-    Course cs405 = new Course("CS405");
-    Course cs418 = new Course("CS418");
-    Course cs428 = new Course("CS428");
-    Course cs452 = new Course("CS452");
-    Course cs455 = new Course("CS455");
-    Course cs460 = new Course("CS460");
-    Course cs465 = new Course("CS465");
-    Course cs470 = new Course("CS470");
-    Course cs478 = new Course("CS478");
-    Course cs486 = new Course("CS486");
-    Course cs493r = new Course("CS493R");
-    Course cs494 = new Course("CS494");
-    Course cs497r = new Course("CS497R");
-    Course cs498r = new Course("CS498R");
+    Course cs260 = new Course("CS260", "cpp", "other",  "Introduction to web application design and implementation. Both front end and back end development with an emphasis on REST architectures.");
+    Course cs330 = new Course("CS330",  "cpp", "other", "Principles and concepts characterizing high-level computer programming languages, process and data abstraction, encapsulation, inheritance, functional programming, logic programming, scanners, and parsers.");
+    Course cs345 = new Course("CS345",  "cpp", "other", "Principles and concepts of operating systems design and the implementation of an operating system.");
+    Course cs355 = new Course("CS355", "cpp", "other",  "Introduces basic concepts of computer graphics, user interfaces and image processing. Includes homogenous coordinate transforms, image transforms, event driven programming and color models. Extends Model-View-Controller architecture.");
+    Course cs405 = new Course("CS405",  "cpp", "other", "Students think critically about the steps necessary to start and run successful Venture Startups.\"");
+    Course cs418 = new Course("CS418", "cpp", "other",  "Examines computational methods for analyzing biological systems. Dynamic programming, Markov models, Neural Networks and Bayesian analysis are used to predict secondary structure, tertiary structure and active sites for drug docking given molecular DNA sequence analysis. Prerequisites: CS 312");
+    Course cs428 = new Course("CS428", "cpp", "other",  "Analysis, design, implementation, and testing of significant software systems.");
+    Course cs452 = new Course("CS452", "cpp", "other",  "Database models: relational, deductive, object-oriented. Integrity constraints, query languages, database design");
+    Course cs455 = new Course("CS455",  "cpp", "other", "Interactive computer graphics systems programming and architecture.");
+    Course cs460 = new Course("CS460", "cpp", "other",  "Introduction to data communications and computer networking. Communications fundamentals, computer networks, software, architecture, telecommunications, regulation, standard");
+    Course cs465 = new Course("CS465",  "cpp", "other", "Introduction to computer security fundamentals: confidentiality, integrity, authentication, and access control. Secret key and public key cryptography, network security protocols, viruses, and fire walls.");
+    Course cs470 = new Course("CS470", "cpp", "other",  "Introduction to core areas of artificial intelligence; intelligent agents, problem solving and search, knowledge-based systems and inference, planning, uncertainty, learning, and perception.");
+    Course cs478 = new Course("CS478", "cpp", "other",  "Machine learning and data mining models and other mechanisms allowing computers to learn and find knowledge from data.");
+    Course cs486 = new Course("CS486",  "cpp", "other", "Verification & Validation");
+    Course cs493r = new Course("CS493R", "cpp", "other",  "Computing Competitions");
+    Course cs494 = new Course("CS494",  "cpp", "other", "Culminating experience based on skills learned in advanced technical courses. Students work in teams to plan, design, test, and demonstrate a major project.");
+    Course cs497r = new Course("CS497R", "cpp", "other",  "Undergraduate Research");
+    Course cs498r = new Course("CS498R", "cpp", "other",  "Students gain hands-on experience solving cybersecurity problems.");
 
     String[] subjects =
             {
@@ -86,5 +86,9 @@ public class CoursesActivity extends AppCompatActivity  {
         DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(recyclerView.getContext(),
                 LinearLayoutManager.VERTICAL);
         recyclerView.addItemDecoration(dividerItemDecoration);
+    }
+
+    protected void setUpCourses() {
+
     }
 }
